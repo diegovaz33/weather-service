2d8f555b1a6259e2048c177440f866b507412bb8
System Administrator <root@Diegos-MacBook-Pro.local>
2024-03-30 11:57:16 -0500

    Fix variable names

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -6,7 +6,7 @@ const BASE_URL = 'https://api.openweathermap.org/data/3.0/onecall';
 
 const getWeather = async (latitude, longitude) => {
     try {
-        const response = await axios.get(`${BASE_URL}?lat=${lat}&lon=${lon}&appid=${openWeatherApiKey}&units=metric`)
+        const response = await axios.get(`${BASE_URL}?lat=${latitude}&lon=${longitude}&appid=${openWeatherApiKey}&units=metric`)
         return utils.processWeatherData(response.data);
     } catch (error) {
         throw new Error('Failed to retrieve data')
